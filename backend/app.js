9e959fa4785d5febc4fca0f28c61176777e4dded
NikkitoPlay <marcos.nikkito@gmail.com>
2024-09-23 16:18:30 -0400

    init app

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,15 @@
-const express = require('express');
-const path = require('path')
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const port = 5000;
+
+const app = express()
+
+//config respostas em JSON e form data 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.listen(port, ()=>{
+  console.log(`App rodando na porta ${port}`)
+})
