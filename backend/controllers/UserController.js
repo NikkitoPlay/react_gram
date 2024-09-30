268827c2c7a141e3537ea113eee3a267bb415f99
NikkitoPlay <marcos.nikkito@gmail.com>
2024-09-30 08:25:23 -0400

    user auth

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,11 +7,70 @@ const jwtSecret = process.env.JWT_SECRET;
 
 //generate user token
 const generateToken = (id) => {
-  return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" }); 
+  return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
 };
 
-const register = async (req, res ) => {
-  res.send('Registro');
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //check se usuario existe
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro email"] });
+    return;
+  }
+
+  // generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  //se usuario foi criado com sucesso
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Por favor, tente novamente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({ _id: newUser._id, token: generateToken(newUser._id) });
+};
+
+
+//login do usuario
+const login = async (req,res) => {
+
+  const {email,password} = req.body;
+
+  const user = await User.findOne({email});
+
+  if(!user){
+    res.status(404).json({errors:["Usuário não encontrado."]})
+    return
+  }
+
+  //check if password matches
+  if(!(await bcrypt.compare(password, user.password))){
+    res.status(422).json({errors:["A senha está errada."]})
+    return
+  }
+
+  //retorn usuário com token
+  res.status(201).json({
+    _id:user._id,
+    profileImage: user.profileImage,
+    token:generateToken(user._id)
+  })
+
+
+   
 }
 
-module.exports = {register, generateToken};
+module.exports = { register, login };
--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -21,12 +21,23 @@ const userCreateValidation = () => {
       .isString()
       .withMessage("A confirmação de senha é obrigatória.")
       .custom((value, { req }) => {
-        if(value != req.body.password){
-          throw new Error("As senhas não são iguais.")
+        if (value != req.body.password) {
+          throw new Error("As senhas não são iguais.");
         }
         return true;
       }),
   ];
 };
 
-module.exports = { userCreateValidation };
+const loginValidation = () => {
+  return [
+    body("email")
+      .isString()
+      .withMessage("O email é obrigatório.")
+      .isEmail()
+      .withMessage("Insira um e-mail válido."),
+    body("password").isString().withMessage("A senha é obrigatória."),
+  ];
+};
+
+module.exports = { userCreateValidation, loginValidation };
