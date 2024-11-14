7b5fd0909bf79044d3f2e36526b9c6896d26fb95
NikkitoPlay <marcos.nikkito@gmail.com>
2024-11-14 11:00:33 -0400

    insert photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,38 +1,35 @@
-const Photo = require("../models/Photo")
-const User = require("../models/User")
+const Photo = require("../models/Photo");
+const User = require("../models/User");
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 //insert a photo with a user
-const insertPhoto = async(req,res)=>{
-  const {title} = req.body;
+const insertPhoto = async (req, res) => {
+  const { title } = req.body;
   const image = req.file.filename;
 
-  const reqUser = req.user
+  const reqUser = req.user;
 
-  const user = User.findById(reqUser._id);
+  const user = await User.findById(reqUser._id);
 
-  console.log(user)
-
+  //console.log(user.name)
   //create a photo
   const newPhoto = await Photo.create({
     image,
     title,
     userId: user._id,
     userName: user.name,
-  })
+  });
 
-  
-
-  if(!newPhoto){
+  if (!newPhoto) {
     res.status(422).json({
-      errors:["Houve um problema, tente novamente mais tarde."]
-    })
+      errors: ["Houve um problema, tente novamente mais tarde."],
+    });
   }
 
-  res.status(201).json(newPhoto)
-}
+  res.status(201).json(newPhoto);
+};
 
 module.exports = {
   insertPhoto,
-}
+};
