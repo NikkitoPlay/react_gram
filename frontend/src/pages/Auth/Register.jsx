9a18fde97fb2b4f0ea65e67db9e8ba4e5d37f6a5
NikkitoPlay <marcos.nikkito@gmail.com>
2024-12-02 16:54:37 -0400

    register-form

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,8 +1,31 @@
 import "./Auth.css"
 
+//components
+import {Link} from "react-router-dom"
+
+//hooks
+import {useState, useEffect} from "react"
+
+
 const Register = () => {
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+      </form>
+      <p>Já tem conta? <Link to='/login'>Clique aqui!</Link></p>
+
+    </div>
   )
 }
 
