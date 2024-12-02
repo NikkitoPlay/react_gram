8b754c08fe0957930a94d2ea05b0cbaec2fadc31
NikkitoPlay <marcos.nikkito@gmail.com>
2024-12-02 16:35:45 -0400

    css navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,37 @@
-import './Navbar.css'
+import "./Navbar.css";
 
 //components
-import { NavLink, Link } from 'react-router-dom'
-import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from 'react-icons/bs'
+import { NavLink, Link } from "react-router-dom";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <nav id="nav">
-      <Link to='/'>ReacGram</Link>
+      <Link to="/">ReacGram</Link>
       <form>
-        <BsSearch/>
-        <input type="text" />
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar..." />
       </form>
       <ul id="nav-links">
-        <NavLink to="/"><BsHouseDoorFill/></NavLink>
-        <NavLink to="/login">Entrar</NavLink>
-        <NavLink to="/register">Cadastrar</NavLink>
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
